22c6797aa83cb381182a8f8efdb200705ad3b42d
Matteo Merli <mmerli@apache.org>
2022-10-12 21:45:15 -0700

    Build wheel files in CI (#11)
    ### Motivation
    
    Added CI jobs to build wheel files for Mac and Linux. 
    
    ### Modifications
    
     * Simplified `CMakeList.txt` for easier detection of Boost-Python
     * Added PR validation to build wheels for small number of combinations
     * Added job triggered on when a tag is pushed to create all the combination of wheels for Py (3.7, 3.8, 3.9, 3.10), OS (Mac, linux glibc, linux musl) and CPU (x86_64 and arm64). 
     * The wheels binaries are tested to verify no libraries/symbols are missing
     * The wheels are uploaded as artifacts for the job, so that they can later be staged for official release.

---
+++ pkg/build-wheel-inside-docker.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -e -x
+
+cd /pulsar-client-python
+
+build-support/install-cpp-client.sh
+
+rm -f CMakeCache.txt CMakeFiles
+
+cmake . \
+      -DCMAKE_BUILD_TYPE=Release
+
+make -j4
+
+./setup.py bdist_wheel
+
+# Audit wheel is required to convert a wheel that is tagged as generic
+# 'linux' into a 'multilinux' wheel.
+# Only wheel files tagged as multilinux can be uploaded to PyPI
+# Audit wheel will make sure no external dependencies are needed for
+# the shared library and that only symbols supported by most linux
+# distributions are used.
+auditwheel repair dist/pulsar_client*-$PYTHON_SPEC-linux_*.whl
---
+++ pkg/mac/build-mac-wheels.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -e -x
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+cd "${ROOT_DIR}"
+
+source pkg/mac/common.sh
+
+PYTHON_CLIENT_VERSION=$(cat version.txt | xargs)
+
+PYTHON_VERSION=$1
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+PREFIX=${CACHE_DIR_DEPS}/install
+
+echo '----------------------------------------------------------------------------'
+echo '----------------------------------------------------------------------------'
+echo '----------------------------------------------------------------------------'
+echo "Build wheel for Python $PYTHON_VERSION"
+
+cd "${ROOT_DIR}"
+
+rm -f CMakeCache.txt
+
+PY_EXE=$PREFIX/bin/python3
+PIP_EXE=$PREFIX/bin/pip3
+
+ARCHS='arm64;x86_64'
+PIP_TAG='universal2'
+if [ $PYTHON_VERSION = '3.7' ]; then
+    ARCHS='x86_64'
+    PIP_TAG=$ARCHS
+fi
+
+cmake . \
+        -DCMAKE_OSX_ARCHITECTURES=${ARCHS} \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_PREFIX_PATH=$PREFIX \
+        -DCMAKE_CXX_FLAGS=-I$PREFIX/include \
+        -DLINK_STATIC=ON \
+        -DPULSAR_LIBRARY=${CACHE_DIR_CPP_CLIENT}/install/lib/libpulsar.a \
+        -DPULSAR_INCLUDE=${CACHE_DIR_CPP_CLIENT}/install/include \
+        -DPython3_ROOT_DIR=$PREFIX \
+        -DBOOST_ROOT=${PREFIX}
+
+make clean
+make -j16 VERBOSE=1
+
+$PY_EXE setup.py bdist_wheel
+
+PY_SPEC=$(echo $PYTHON_VERSION | sed 's/\.//g')
+
+cd /tmp
+$PIP_EXE install --no-dependencies --force-reinstall \
+    $ROOT_DIR/dist/pulsar_client-${PYTHON_CLIENT_VERSION}-cp$PY_SPEC-*-macosx_10_15_${PIP_TAG}.whl
+$PY_EXE -c 'import pulsar'
+
