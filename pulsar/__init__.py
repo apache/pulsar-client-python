e282af9e618221ecd3248a2ea16108b9f8bcde70
Eric Hare <ericrhare@gmail.com>
2023-02-06 14:22:26 -0800

    Issue #66 - add a string representation of a MessageId object

--- pulsar/__init__.py
+++ pulsar/__init__.py
@@ -102,6 +102,12 @@ class MessageId:
         """
         return self._msg_id.serialize()
 
+    def __str__(self):
+        return "MessageId instance with entry_id %s and ledger_id %s" % (str(self.entry_id), str(self.ledger_id))
+
+    def __repr__(self):
+        return "MessageId instance with entry_id %s and ledger_id %s" % (str(self.entry_id), str(self.ledger_id))
+
     @staticmethod
     def deserialize(message_id_bytes):
         """
