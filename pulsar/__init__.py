2aaacad8b13f2498a47d35554a7eec19a7579be2
Eric Hare <ericrhare@gmail.com>
2023-02-16 08:18:33 -0700

    Issue #37 : Allow passing pulsar.MessageId instance to create_reader() (#95)

--- pulsar/__init__.py
+++ pulsar/__init__.py
@@ -882,6 +882,11 @@ class Client:
             Symmetric encryption class implementation, configuring public key encryption messages for the producer
             and private key decryption messages for the consumer
         """
+        
+        # If a pulsar.MessageId object is passed, access the _pulsar.MessageId object
+        if isinstance(start_message_id, MessageId):
+            start_message_id = start_message_id._msg_id
+
         _check_type(str, topic, 'topic')
         _check_type(_pulsar.MessageId, start_message_id, 'start_message_id')
         _check_type(_schema.Schema, schema, 'schema')
