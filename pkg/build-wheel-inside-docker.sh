d5b67903eda7c66731fe4d58a9f550a8c86b4238
Yunze Xu <xyzinfernity@163.com>
2025-01-07 17:23:21 +0800

    Build wheels via vcpkg on manylinux2014

--- pkg/build-wheel-inside-docker.sh
+++ pkg/build-wheel-inside-docker.sh
@@ -22,6 +22,33 @@ set -e -x
 
 cd /pulsar-client-python
 
+# Build cpp wheels
+if [[ $ARCH == "aarch64" ]]; then
+    export VCPKG_FORCE_SYSTEM_BINARIES=1
+fi
+PULSAR_CPP_VERSION=$(dep-version.py pulsar-cpp)
+. /dep-url.sh
+download_dependency /dependencies.yaml pulsar-cpp
+cd apache-pulsar-client-cpp-${PULSAR_CPP_VERSION}
+
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+# The following dependencies are required to build vcpkg on arm64 Linux
+yum install -y curl zip unzip tar perl-IPC-Cmd
+git clone https://github.com/ninja-build/ninja.git
+cd ninja
+git checkout release
+/opt/python/cp312-cp312/bin/python configure.py --bootstrap
+mv ninja /usr/bin/
+cd ..
+./bootstrap-vcpkg.sh
+cd ..
+
+cmake -B build -DINTEGRATE_VCPKG=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_DYNAMIC_LIB=ON -DBUILD_STATIC_LIB=ON
+cmake --build build -j8 --target install
+cd ..
+rm -rf apache-pulsar-client-cpp-$(PULSAR_CPP_VERSION)
+
 PYBIND11_VERSION=$(./build-support/dep-version.py pybind11)
 curl -L -O https://github.com/pybind/pybind11/archive/refs/tags/v${PYBIND11_VERSION}.tar.gz
 tar zxf v${PYBIND11_VERSION}.tar.gz
